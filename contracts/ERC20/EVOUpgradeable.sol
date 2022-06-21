bb11ca2e525819e79a2beeaaa94588ac3e067fcf
Nicholas St. Germain <nick@cajun.pro>
2022-06-20 19:38:37 -0500

    deploy EVO to 6 additional chains

--- contracts/ERC20/EVOUpgradeable.sol
+++ contracts/ERC20/EVOUpgradeable.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
-import "@routerprotocol/router-crosstalk/contracts/RouterCrossTalkUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract EVO is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20CappedUpgradeable,
-PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, RouterCrossTalkUpgradeable {
+/**
+* @title EVO v1.0.0
+* @author @DirtyCajunRice
+*/
+contract EVOUpgradeable is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20CappedUpgradeable,
+PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
@@ -27,21 +30,17 @@ PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, RouterCro
         __Pausable_init();
         __AccessControl_init();
         __ERC20Permit_init("EVO");
-        // Generic Handlers for RCT:
-        // Polygon: 0xa526Cc7B3a12E2edbaf81471191900Ca9D37B3d9
-        //
-        __RouterCrossTalkUpgradeable_init("asdf");
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
+    function pause() public onlyRole(ADMIN_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyRole(ADMIN_ROLE) {
         _unpause();
     }
 
@@ -49,20 +48,11 @@ PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, RouterCro
         _mint(to, amount);
     }
 
-    // Router CrossTalk Functions
-    function setRCTLink(address _address) public onlyRole(ADMIN_ROLE) {
-        setLink(_address);
+    function _mint(address to, uint256 amount) internal override(ERC20Upgradeable, ERC20CappedUpgradeable) {
+        super._mint(to, amount);
     }
 
-    function setRCTFeeToken(address _address) public onlyRole(ADMIN_ROLE) {
-        setRCTFeeToken(_address);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-    internal
-    whenNotPaused
-    override
-    {
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
